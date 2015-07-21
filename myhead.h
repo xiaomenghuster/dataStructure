b884423788250a8325372744b80769eb83e0f1e8
xiaomenghuster <xiaomeng@hust.edu.cn>
2015-07-21 21:59:28 +0800

    success with match_3 and can find more than one in 100000 character

--- myhead.h
+++ myhead.h
@@ -1,8 +1,11 @@
 #define CONTENTFILE   "ascii.txt"
 #define ERROR  -1
+#define TRUE 1
+#define FALSE 0
 typedef struct node{
 	char ch;
 	struct node *next;
+	struct node *pre;
 }node;
 void start();  
 int init(node*);  //初始化，将文件中的字符串读取到链表中
@@ -12,5 +15,6 @@ void create(long number); //产生数据文件
 long match_1(node* head,char* str,long ppos);  //匹配算法函数，传入需要匹配的字符串
 long match_2(node* head,char* str,long ppos);
 	void get_next(char* , int*);//
-long match_3(node* head,char* str);
+long match_3(node* head,char* str,long ppos);
+	node* movesteps(node *head,int steps);
 long match_4(node* head,char* str);
